34ade2f754b0496fe5963d8bf040258104d32533
MiklashMark <markmiklash@gmail.com>
2022-12-08 00:57:59 +0300

    kurs
    kursovaya

--- KURSOVAYA/main.cpp
+++ KURSOVAYA/main.cpp
@@ -9,7 +9,8 @@ using namespace std;
 
 string file_Registration = "file_auth.txt";
 string questions = "file_questions.txt";
-
+string Temperament = "Temperament.txt";
+string Information_about_Users = "Users.txt";
 class MyException
 {
 private:
@@ -32,8 +33,59 @@ public:
         string password;
 
         User(){}
+        User(string user_login,string password_hash)
+        {
+            this->login = user_login;
+            this->password = password_hash;
+        }
+        ~User(){}
 
         };
+
+class Set_Temperament
+    {
+private:
+        string Temperament;
+        string log;
+
+public:
+        Set_Temperament(string user_login,string Temperament)
+        {
+            this->log = user_login;
+            this->Temperament = Temperament;
+
+        }
+
+       void Write_Temperament_to_file(Set_Temperament &User_XX)
+       {
+           ofstream ofs;
+           ofs.open(Information_about_Users, ofstream::app);
+
+           if (!ofs.is_open())
+               throw MyException("Ошибка открытия файла");
+
+           ofs << User_XX << endl;
+       }
+   friend ostream &operator<<(ostream &os, Set_Temperament &User_XX)
+    {
+        os << "Логин : " << User_XX.log << endl << User_XX.Temperament <<endl;
+        return os;
+    }
+    friend ofstream &operator<<(ofstream &of, Set_Temperament &User_XX)
+    {
+        of << User_XX.log << endl << User_XX.Temperament;
+        return of;
+    }
+
+    friend istream &operator>>(istream &in, Set_Temperament &User_XX)
+    {
+        in >> User_XX.log;
+        in >> User_XX.Temperament;
+        return in;
+    }
+        ~Set_Temperament()
+        {}
+    };
 
 
  ostream &operator<<(ostream &os, User &User_XX)
@@ -104,25 +156,37 @@ ofstream &operator<<(ofstream &of, User &User_XX)
 
 class Print : public Registration
 {
-private:
+protected:
      string Test1;
-     string Test2;
+
 public:
-    Print(string Test1_, string Test2_)
+    Print(string Test1_)
     {
         Test1 = Test1_;
-        Test2 = Test2_;
 
     }
-
-     void print()
-    {
-        cout<<Test1<<endl<<Test2;
-    }
+    string Test2;
     ~Print()
     {
 
     }
+};
+class Print1 : public Print
+{
+    string Test2;
+public:
+    Print1(string Test1_,string Test2_) : Print(std::move(Test1_))
+    {
+        this->Test2 = Test2_;
+    }
+    void print()
+    {
+        cout<<Test1<<endl<<Test2<<endl;
+    }
+    ~Print1()
+    {}
+
+
 };
 
 
@@ -184,27 +248,40 @@ public:
             cout<<"Выберите одну из 4-ех букв!"<<endl;
             fflush(stdin);
             cin>>answer;
+            if(answer != 'A' && answer != 'a' && answer != 'B' && answer != 'b' && answer != 'V' && answer != 'v' && answer != 'G' && answer != 'g' )
+            {
+                throw MyException("Введите корректный вариант ответа!");
+            }
+
             Que_XX.Answ.push_back(answer);
         }
 
 
     }
-    static  void Counter(Questions &Que_XX)
+    static  string Counter(Questions &Que_XX)
     {
+        string temp1 = "По результатам теста Вы ФЛЕГМАТИК!";
+        string temp2 = "По результатам теста Вы МЕЛАНХОЛИК";
+        string temp3 = "По результатам теста Вы ХОЛЕРИК!";
+        string temp4 = "По результатам теста Вы САНГВИНИК!";
+
         int i = 0;
         while(i!=14)
         {
             if(Que_XX.Answ[i] == 'A' ||  Que_XX.Answ[i] == 'a' )
             {
                 Que_XX.count_A++;
+
             }
             if(Que_XX.Answ[i] == 'B'  || Que_XX.Answ[i] == 'b' )
             {
                 Que_XX.count_B++;
+
             }
             if(Que_XX.Answ[i] == 'V' || Que_XX.Answ[i] == 'v' )
             {
                 Que_XX.count_V++;
+
             }
             if(Que_XX.Answ[i] == 'G' || Que_XX.Answ[i] == 'g' )
             {
@@ -212,8 +289,18 @@ public:
             }
             i++;
         }
+        if(Que_XX.count_A == max(Que_XX.count_A,max(Que_XX.count_B,max(Que_XX.count_V,Que_XX.count_G))))
+        { return temp1;}
+        if(Que_XX.count_B == max(Que_XX.count_A,max(Que_XX.count_B,max(Que_XX.count_V,Que_XX.count_G))))
+        { return temp2;}
+        if(Que_XX.count_V == max(Que_XX.count_A,max(Que_XX.count_B,max(Que_XX.count_V,Que_XX.count_G))))
+        { return temp3;}
+        if(Que_XX.count_G == max(Que_XX.count_A,max(Que_XX.count_B,max(Que_XX.count_V,Que_XX.count_G))))
+        { return temp4;}
 
     }
+
+
     static void Vector_Print(Questions &Que_XX)
     {
         int k = 0;
@@ -240,6 +327,9 @@ public:
     {
 
     }
+
+
+
     template<typename T>
     class List
     {
@@ -367,11 +457,13 @@ public:
                 cout << "Нажмите 1, если вы новый пользователь!" << endl;
                 cout << "Нажмите 2, если вы уже зарегистрированы и хотите войти" << endl;
                 int input;
-                cin >> input;
-                if(input!= 1 && input!=2)
+                while( input!=1 && input!=2)
                 {
-                    throw MyException("Введите корректный вариант!");
-                }
+                    cin >> input;
+                    if(input!=1 && input!=2)
+                    cout<<"Введите корректное значение!"<<endl;
+
+                };
                 if (input == 1)
                 {
                     fflush(stdin);
@@ -417,12 +509,22 @@ public:
                 //--------------------------ТЕСТ---------------------//
                 string Test1_ ="Чтобы выбирать  вариант ответа нажимайте на одну из 4 букв: A, Б, В, Г";
                 string Test2_ = "Нажмите 1 для того, чтобы начать тест ";
-                Print Test(Test1_,Test2_);
+                Print1 Test(Test1_,Test2_);
                 Test.print();
-                Questions Que_01;
-                Questions::ReadQueFromFile(Que_01);
-                Questions::Counter(Que_01);
-                Questions::Counter_Print(Que_01);
+
+                cin>>input;
+                if(input == 1)
+                {
+                    Questions Que_01;
+                    Questions::ReadQueFromFile(Que_01);
+                    string temperament = Questions::Counter(Que_01);
+                    cout<<temperament<<endl;
+                    Set_Temperament User_T(user_login,temperament);
+                    User_T.Write_Temperament_to_file(User_T);
+                    cout<<User_T;
+
+                }
+
 
 
 
